38c2ca2ec47519031da3adabf56df68bfdebce26
byKyMy <kim.broschke@gmail.com>
2021-07-14 17:50:03 +0200

    added Main Class

--- src/main/java/deby_kymy/mlgrush/Mlgrush.java
+++ src/main/java/deby_kymy/mlgrush/Mlgrush.java
@@ -1,15 +1,25 @@
 package deby_kymy.mlgrush;
 
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Mlgrush extends JavaPlugin {
 
+
+    public static String PREFIX = "[§bMlg§fRush]";
     public static Mlgrush instance;
     public static Mlgrush getInstance(){return instance;}
+
+
 
     @Override
     public void onEnable() {
 
+
+        PluginManager pluginManager = Bukkit.getPluginManager();
+
+        Bukkit.getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
 
     }
 
