78520450376909fc46576eb4d4442d349973b487
byKyMy <kim.broschke@gmail.com>
2021-07-17 23:37:18 +0200

    changed plugin version to 1.17.1

--- src/main/java/deby_kymy/mlgrush/Mlgrush.java
+++ src/main/java/deby_kymy/mlgrush/Mlgrush.java
@@ -1,8 +1,10 @@
 package deby_kymy.mlgrush;
 
 import deby_kymy.mlgrush.Commands.Spawn;
+import deby_kymy.mlgrush.Listener.BreakBed;
 import deby_kymy.mlgrush.Listener.GameStates;
 import deby_kymy.mlgrush.Listener.JoinListener;
+import deby_kymy.mlgrush.Listener.KillCounter;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -25,6 +27,8 @@ public final class Mlgrush extends JavaPlugin {
         PluginManager pluginManager = Bukkit.getPluginManager();
         pluginManager.registerEvents(new JoinListener(), this);
         pluginManager.registerEvents(new GameStates(), this);
+        pluginManager.registerEvents(new BreakBed(), this);
+        pluginManager.registerEvents(new KillCounter(), this);
 
         Objects.requireNonNull(getCommand("mlgrush")).setExecutor(new Spawn());
 
